eadf84b1b5a81f03ea39ac5e7a8b9292d13aea4a
Vadim Kuznetsov <vadim.kuznetsov@mev.com>
2020-09-14 18:10:04 +0300

    accommodation page

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,18 @@ var gulp         = require('gulp'),
     scss         = require('gulp-sass'),
     cleanCSS     = require('gulp-clean-css'),
     autoprefixer = require('gulp-autoprefixer'),
-    notify       = require("gulp-notify");
+    notify       = require("gulp-notify"),
+    browserSync  = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    },
+    notify: false,
+    open: false
+  })
+});
 
 gulp.task('scss', function() {
   return gulp.src('src/style.scss')
@@ -12,10 +23,17 @@ gulp.task('scss', function() {
     }))
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('html', function() {
+  return gulp.src('./*.html')
+    .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/style.scss', gulp.parallel('scss'));
+  gulp.watch('src/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('./*.html', gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('scss', 'watch'));
+gulp.task('default', gulp.parallel('scss', 'browser-sync', 'watch'));
